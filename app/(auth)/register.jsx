8b7da80de77684af30bbc2f750f567291797ab49
harunaa <haruna@globavate.com>
2025-04-14 13:19:57 +0000

    add a pressable componenet button

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -7,8 +7,14 @@ import ThemedLogo from "../../components/ThemedLogo";
 import Spacer from "../../components/Spacer";
 import ThemedText from "../../components/ThemedText";
 import { Link } from 'expo-router';
+import ThemedButton from '../../components/ThemedButton';
 
 const Register = () => {
+
+    const handleSubmit = () => {
+        console.log("register form submitted success")
+    }
+
     return (
         <ThemedView style={styles.container}>
 
@@ -16,6 +22,10 @@ const Register = () => {
             <ThemedText title={true} style={styles.title}>
                 Register Your Account
             </ThemedText>
+
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{ color: "#f2f2f2" }}>Register</Text>
+            </ThemedButton>
 
             <Spacer height={100} />
             <Link href="/login">
@@ -33,7 +43,8 @@ export default Register
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: "center"
     },
     title: {
         textAlign: "center",
