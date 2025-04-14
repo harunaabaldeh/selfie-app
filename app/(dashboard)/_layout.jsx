707cba1882e6b4a102a19f338942018049b4725b
harunaa <haruna@globavate.com>
2025-04-14 15:01:43 +0000

    add tab icons

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,6 +1,8 @@
 import { Tabs } from 'expo-router'
 import { StyleSheet, useColorScheme } from 'react-native'
 import { Colors } from '../../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const DashboarLayout = () => {
     const colorScheme = useColorScheme();
@@ -20,15 +22,39 @@ const DashboarLayout = () => {
         >
             <Tabs.Screen
                 name="profile"
-                options={{ title: "Profile" }}
+                options={{
+                    title: "Profile", tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name={focused ? "person" : "person-outline"}
+                            size={24}
+                            color={focused ? theme.iconColorFocused : theme.iconColor}
+                        />
+                    )
+                }}
             />
             <Tabs.Screen
                 name="books"
-                options={{ title: "Books" }}
+                options={{
+                    title: "Books", tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name={focused ? "book" : "book-outline"}
+                            size={24}
+                            color={focused ? theme.iconColorFocused : theme.iconColor}
+                        />
+                    )
+                }}
             />
             <Tabs.Screen
                 name="create"
-                options={{ title: "Create" }}
+                options={{
+                    title: "Create", tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name={focused ? "create" : "create-outline"}
+                            size={24}
+                            color={focused ? theme.iconColorFocused : theme.iconColor}
+                        />
+                    )
+                }}
             />
 
         </Tabs>
