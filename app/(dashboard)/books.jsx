8c8b0c7512c5be4de139b732c8571f55ba5e1b9e
harunaa <haruna@globavate.com>
2025-04-16 13:45:48 +0000

    update books view to show all books in a flat list

--- app/(dashboard)/books.jsx
+++ app/(dashboard)/books.jsx
@@ -1,19 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet } from "react-native";
+import { useBooks } from "../../hooks/useBooks";
+import { Colors } from "../../constants/Colors";
+
 import ThemedView from "../../components/ThemedView";
-import Spacer from '../../components/Spacer';
-import ThemedText from '../../components/ThemedText';
+import Spacer from "../../components/Spacer";
+import ThemedText from "../../components/ThemedText";
+import ThemedCard from "../../components/ThemedCard";
 
 const Books = () => {
+    const { books } = useBooks();
+
     return (
         <ThemedView style={styles.container} safe={true}>
-
             <Spacer />
             <ThemedText title={true} style={styles.heading}>
                 Your Reading List
             </ThemedText>
+
+            <Spacer />
+            <FlatList
+                data={books}
+                keyExtractor={(item) => item.$id}
+                contentContainerStyle={styles.list}
+                renderItem={({ item }) => (
+                    <Pressable>
+                        <ThemedCard style={styles.card}>
+                            <ThemedText style={styles.title}>{item.title}</ThemedText>
+                            <ThemedText style={styles.title}>Written by {item.author}</ThemedText>
+                        </ThemedCard>
+                    </Pressable>
+                )}
+            />
         </ThemedView>
-    )
-}
+    );
+};
 
 export default Books;
 
@@ -21,11 +41,28 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
     },
     heading: {
         fontWeight: "bold",
         fontSize: 18,
-        textAlign: "center"
+        textAlign: "center",
+    },
+    list: {
+        marginTop: 40,
+    },
+    card: {
+        width: "90%",
+        marginHorizontal: "5%",
+        marginVertical: 10,
+        padding: 10,
+        paddingLeft: 14,
+        borderLeftColor: Colors.primary,
+        borderLeftWidth: 4
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 10
     }
-})
+});
