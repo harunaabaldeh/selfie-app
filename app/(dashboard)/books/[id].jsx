e4529ed2a3b0b585aeab58108ef48bfaed121dfc
harunaa <haruna@globavate.com>
2025-04-16 15:34:31 +0000

    fetch a single book

--- app/(dashboard)/books/[id].jsx
+++ app/(dashboard)/books/[id].jsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native'
 import { useLocalSearchParams } from 'expo-router';
+import { useBooks } from '../../../hooks/useBooks'
 
 
 // themed imports
@@ -7,12 +9,43 @@ import ThemedView from "../../../components/ThemedView";
 import Spacer from "../../../components/Spacer";
 import ThemedText from "../../../components/ThemedText";
 import ThemedCard from "../../../components/ThemedCard";
+import ThemedLoader from "../../../components/ThemedLoader";
 
 const MoreDetails = () => {
+    const [book, setBook] = useState(null);
+
     const { id } = useLocalSearchParams();
+    const { fetchBookById } = useBooks();
+
+    useEffect(() => {
+        async function loadBook(id) {
+            const bookData = await fetchBookById(id);
+            setBook(bookData);
+        }
+
+        loadBook(id);
+    }, [id])
+
+    if (!book) {
+        return (
+            <ThemedView safe={true} styles={styles.container}>
+                <ThemedLoader />
+            </ThemedView>
+        )
+    }
+
     return (
         <ThemedView safe={true} styles={styles.container}>
-            <ThemedText>Book Details - {id}</ThemedText>
+            <ThemedCard style={styles.card}>
+                <ThemedText style={styles.title}>{book.title}</ThemedText>
+                <ThemedText>Written by {book.author}</ThemedText>
+                <Spacer />
+
+                <ThemedText title={true}>Book Discription:</ThemedText>
+                <Spacer height={10} />
+
+                <ThemedText>{book.description}</ThemedText>
+            </ThemedCard>
         </ThemedView>
     )
 }
@@ -23,5 +56,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'stretch',
+    },
+    title: {
+        fontSize: 22,
+        marginVertical: 10,
+    },
+    card: {
+        margin: 20
     }
 })
